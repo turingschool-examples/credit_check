7f7379a4ee83da584677259c338bdd4c9fc92089
Vince Carollo <vincecarollo@gmail.com>
2019-03-20 13:21:35 -0600

    Finish luhn algorithm

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,7 +1,33 @@
-card_number = "5541808923795240"
-
+#invalid test
+# card_number = "5554180192379524040240071065123806011797668868728"
+#valid test
+card_number = "554180892379524040240071365123806011797668867828"
 # Your Luhn Algorithm Here
+luhn_output = card_number.split('').map.with_index do |num, i|
+  if i.even?
+    even_i_num = num.to_i * 2
+    if even_i_num > 9
+      even_i_num = even_i_num.to_s.split('')
+    end
+    even_i_num
+  else
+    num.to_i
+  end
+end
 
+luhn_output.map! do |num|
+  if num.class == Array
+    num.inject(0){|sum,x| sum + x.to_i }
+  else
+    num
+  end
+end
+
+if luhn_output.inject(0){|sum, x| sum + x} % 10 == 0
+  p "The number #{luhn_output.join} is valid!"
+else
+  p "The number #{luhn_output.join} is invalid!"
+end
 # Output
 ## If it is valid, print "The number [card number] is valid!"
 ## If it is invalid, print "The number [card number] is invalid!"
