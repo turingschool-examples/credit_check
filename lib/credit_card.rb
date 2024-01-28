95bdba0a138e3438d7eb8811f9dc66b38e19534f
Logan Matheny <loganpaulmatheny@gmail.com>
2024-01-27 21:56:46 -0500

    Functions for CreditCard class

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,1 +1,28 @@
-# pass two arguments on initialization 
+# pass two arguments on initialization (string of the credit card number; integer representing the limit)
+require './lib/credit_check'
+
+class CreditCard 
+  attr_reader :card_number, :limit 
+
+  def initialize(card_number, limit)
+    @card_number =card_number
+    @limit = limit
+  end 
+
+  def card_number
+    @card_number
+  end
+
+  def limit
+    @limit
+  end
+
+  def is_valid?
+    valid_card(@card_number) == 0 ? true : false
+  end
+
+  def last_four
+    # returns a string of the last four digits
+  end 
+
+end
